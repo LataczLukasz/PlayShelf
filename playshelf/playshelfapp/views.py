8df9b01a9e34f2c75dccf86911b902698786bf62
Jakub <kubajj7@proton.me>
2024-12-10 17:02:13 +0100

    feature/REST-API/use-serializers

--- playshelf/playshelfapp/views.py
+++ playshelf/playshelfapp/views.py
@@ -2,6 +2,10 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest, JsonResponse
 from .models import Game
+from .serializers import GameSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 def home(request):
     assert isinstance(request, HttpRequest)
@@ -38,19 +42,11 @@ def about(request):
         }
     )
 
+@api_view(['GET'])
 def games_list(request):
     games = Game.objects.all()
     if request.headers.get('Accept') == 'application/json':
-        games_data = [
-            {
-                'title': game.title,
-                'developer': game.developer,
-                'publisher': game.publisher,
-                'release_date': game.release_date.strftime('%Y-%m-%d'),
-                'average_rating': game.average_rating,
-                'description': game.description,
-            }
-            for game in games
-        ]
-        return JsonResponse({'games': games_data})
+        serializer = GameSerializer(games, many=True)
+        return Response({'games': serializer.data})
     return render(request, 'playshelfapp/games_list.html', {'games': games})
+
